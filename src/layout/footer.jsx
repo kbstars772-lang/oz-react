e2582cd89febaebf0f4c0a36e26f4395e0d67d08
문성주 <www.sjm2118@naver.com>
2026-04-02 16:39:47 +0900

    Child

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-    return <h1>footer</h1>;
-};
-
-export default Footer;
+    return <h1>Footer</h1>;
+  };
+  
+  export default Footer;
--- src/layout/headder.jsx
+++
@@ -1,5 +0,0 @@
-const Headder = () => {
-    return <h1>my app</h1>;
-};
-
-export default Headder;
